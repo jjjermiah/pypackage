3db3a3853fd2864aa31b815646c97ad11af2e599
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-05-16 17:48:00 -0400

    feat: add ruff

--- src/testmypixipkg/__main__.py
+++ src/testmypixipkg/__main__.py
@@ -1,11 +1,15 @@
+"""This module contains the main functionality of the testmypixipkg package."""
+
 from rich import print
 
 
-def hello():
+def hello() -> tuple[str, str]:
+    """Return a greeting message and an emoji."""
     return "Hello, [bold magenta]World[/bold magenta]!", ":vampire:"
 
 
-def say_hello():
+def say_hello() -> None:
+    """Print a greeting message and an emoji."""
     print(*hello())
 
 
